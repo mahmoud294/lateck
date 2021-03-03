db46d0b0ccbafba31f01a1747b98de8942ea19b0
mahmoud294 <mahmoud.matrix94@gmail.com>
2021-03-03 16:35:08 +0200

    finish

--- lib/screens/cartScreen.dart
+++ lib/screens/cartScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lateck/providers/cart.dart';
 import 'package:lateck/widgets/cartView.dart';
+import 'package:provider/provider.dart';
 
 class CartScreen extends StatefulWidget {
   static const routeName = '/cart';
@@ -10,14 +12,71 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.black,size: 30),
+    final tot = Provider.of<Cart>(context).totalAmount;
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black, size: 30),
         backgroundColor: Colors.white,
         elevation: 0,
-      ),body: Column(
+      ),
+      body: Column(
         children: [
           CartItemsView(),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: ListView(
+                children: <Widget>[
+                  Card(
+                    child: Container(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            'Total',
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.w700),
+                          ),
+                          Expanded(
+                            child: Container(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                '${tot.toStringAsFixed(2)} \$',
+                                style:
+                                    TextStyle(color: Colors.blue, fontSize: 15),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Container(
+            height: 80,
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                color: Colors.blue,
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  'Pay',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ),
         ],
-      ),);
+      ),
+    );
   }
 }
